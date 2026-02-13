0f96c8493f9252069be592e451b69fbc94c831c3
N0ctane._.Dev <n0ctaneDev@gmail.com>
2026-02-13 08:28:04 +0530

    Update 3-essential.sh

---
+++ installers/3-essential.sh
@@ -0,0 +1,7 @@
+#!/bin/env bash
+
+# here we install additional pkgs that aren't available on pacman/AUR repos
+
+# install oh-my-posh (shell prompt)
+# config will be loaded in [3.2-configs.sh]
+curl -s https://ohmyposh.dev/install.sh | bash -s
